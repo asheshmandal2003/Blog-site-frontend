8f4cf5ada2e9277b6c340b4b12964d5e4dbb4ef2
Ashesh Mandal <116034358+asheshmandal2003@users.noreply.github.com>
2023-02-02 02:10:27 +0530

    added media query

--- src/components/write/Write.jsx
+++ src/components/write/Write.jsx
@@ -1,38 +1,40 @@
 import "./write.css";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function Write() {
   return (
     <div className="write">
-      <div className="writeImg">
-        <img
-          src="https://images.pexels.com/photos/545042/pexels-photo-545042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
-      </div>
-      <form className="writeForm">
-        <div className="writeFormGroup">
-          <label htmlFor="fileInput">
-            <span className="writeIcon">
-              <ion-icon name="add-circle-outline"></ion-icon>
-            </span>
-          </label>
-          <input type="file" id="fileInput" style={{ display: "none" }} />
-          <input
-            type="text"
-            placeholder="Title..."
-            className="writeInput"
-            autoFocus={true}
+      <div className="write-wrapper">
+        <div className="writeImg">
+          <img
+            src="https://images.pexels.com/photos/545042/pexels-photo-545042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
           />
         </div>
-        <div className="writeFormGroup">
-          <textarea
-            placeholder="Write your story..."
-            className="writeInput writeText"
-            type="text"
-          ></textarea>
-        </div>
-        <button className="writeSubmit">Publish</button>
-      </form>
+        <form className="writeForm">
+          <div className="writeFormGroup">
+            <label htmlFor="fileInput">
+              <span className="writeIcon">
+                <ion-icon name="add-circle-outline"></ion-icon>
+              </span>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+            <input
+              type="text"
+              placeholder="Title..."
+              className="writeInput"
+              autoFocus={true}
+            />
+          </div>
+          <div className="writeFormGroup">
+            <TextareaAutosize
+              placeholder="Write your story..."
+              className="writeText"
+            />
+          </div>
+          <button className="writeSubmit">Publish</button>
+        </form>
+      </div>
     </div>
   );
 }
